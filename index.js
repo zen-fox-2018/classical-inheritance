7d83d63b5b81bb5d9bb1d216525b1c859af78e78
Arief Rachman <arievrchman@gmail.com>
2018-12-03 23:51:25 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 "use strict"
+
+class Animals {
+    constructor() {
+        this.legs = 4;
+        this.eyes = 2;
+    }
+    
+}
+
+class Frog extends Animals {
+    constructor() {
+        super();
+        this.eyes = 3;
+    }
+    
+    
+}
+
+class Fox extends Animals {
+    constructor() {
+        super();
+        this.tails = 9
+        this.ability = new superPower()
+    }
+}
+
+class superPower {
+    constructor() { }
+    
+    abilities() {
+        return 'be Invisible'
+    }
+}
+
+// TEST PARENT
+const parents = new Animals()
+console.log(parents)
+
+// TEST CHILD
+const katak = new Frog()
+const zen = new Fox()
+console.log(katak)
+console.log(zen.ability.abilities())
