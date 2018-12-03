f91025258cbcd590042d2c55580ecd4cf88d1635
fauzan_bintang <obin.aji@gmail.com>
2018-12-03 18:28:28 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 "use strict"
+class Animal {
+    constructor() {
+        this.num_legs = 4,
+        this.num_eyes = 2
+        this.superpower = new SuperPower()
+    }
+}
+
+class Fox extends Animal {
+    constructor() {
+        super()
+    }
+}
+
+class Dog extends Animal {
+    constructor() {
+        super()
+    }
+}
+
+class Chicken extends Animal {
+    constructor() {
+        super()
+        this.num_legs = 2
+        this.num_wings = 2
+    }
+}
+
+class SuperPower {
+    be_invisible() {
+        return 'im invisible'
+    }
+    flying_kick() {
+        return 'ciiiat.....'
+    }
+}
+
+let fox = new Fox()
+let dog = new Dog()
+let chicken = new Chicken()
+
+console.log(fox.superpower.be_invisible());
+console.log(dog);
+console.log(chicken.superpower.flying_kick());
